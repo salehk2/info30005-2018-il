14a86deb0e8a5af61a105a0c8040c9add06eade8
sak2 <sahmedk2@outlook.com>
2018-05-10 17:34:03 +1000

    Heroku backend test

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+var assert = require('assert');
+require("../models/post");
+require('../models/db');
+var controller = require('../controller/controller');
+
+var url = 'mongodb://sak2:sak474@ds014648.mlab.com:14648/illuminate';
+
+/* GET home page. */
+// router.get('/', controller.index);
+
+router.get('/forum', controller.viewAll);
+
+router.get('/submit', controller.submit);
+
+router.post('/submit', controller.createPost);
+
+router.post('/remove', controller.removePost);
+
+router.get('/forum/:id', controller.viewPost);
+
+router.get('/forum/:id/edit?', controller.editor);
+
+router.post('/forum/:id/edit?', controller.edit);
+
+module.exports = router;
