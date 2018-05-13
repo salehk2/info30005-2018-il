82e5f61e6f7fceac6016df30030f717d3ec3ec8f
sak2 <sahmedk2@outlook.com>
2018-05-13 18:12:36 +1000

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,52 @@
-var express = require('express');
-var router = express.Router();
-var mongo = require('mongodb').MongoClient;
-var objectId = require('mongodb').ObjectID;
-var assert = require('assert');
+const express = require('express');
+const router = express.Router();
+const mongo = require('mongodb').MongoClient;
+const objectId = require('mongodb').ObjectID;
 require("../models/post");
 require('../models/db');
-var controller = require('../controller/controller');
+const controller = require('../controller/controller');
 
-var url = 'mongodb://sak2:sak474@ds014648.mlab.com:14648/illuminate';
+const url = 'mongodb://sak2:sak474@ds014648.mlab.com:14648/illuminate';
 
-/* GET home page. */
+/* GET the home page. This contains links to the parts of the projects */
 router.get('/', controller.home);
 
+
+/* Because the purpose of this week's deliverable was to show that
+   we can build a fully functional backend that uses REST and the
+   database,
+   we get all the other planned functionality and redirect it
+   to a coming soon page. (There is nothing more required for this
+   week's deliverable that we had left to demonstrate in them,
+   it'll be done by next week :)
+ */
+router.get('/about', function(req, res) {
+    res.redirect('/coming-soon');
+});
+router.get('/wiki', function(req, res) {
+    res.redirect('/coming-soon');
+});
+router.get('/petitions', function(req, res) {
+    res.redirect('/coming-soon');
+});
+router.get('/coming-soon', controller.pageUnderConstruction);
+
+/* the remaining GET requests */
 router.get('/forum', controller.viewAll);
 
 router.get('/submit', controller.submit);
-
-router.post('/submit', controller.createPost);
-
-router.post('/remove', controller.removePost);
 
 router.get('/forum/:id', controller.viewPost);
 
 router.get('/forum/:id/edit?', controller.editor);
 
-// router.post('/forum/:id/edit?', controller.edit);
+/* removing an existing post, submitting a new post and
+   updating posts, done via the following POST requests
+ */
+router.post('/remove', controller.removePost);
+
+router.post('/submit', controller.createPost);
+
 router.post('/update', controller.edit);
 
 module.exports = router;
