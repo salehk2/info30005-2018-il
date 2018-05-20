e372c3d43e843f44a954244274fe77079fcb621a
salehk2 <37172006+salehk2@users.noreply.github.com>
2018-05-20 16:37:24 +1000

    Delete routes.js

--- routes/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const controller = require("../controllers/comingsoonController")
-
-
-router.get('/', controller.sayHello)
-
-module.exports = router;
